2485c9fb489adbdf656639cae97eb98fd2e4aa75
bbassett <bbassett@kent.edu>
2019-09-23 12:05:11 -0400

    test34

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Bryan James Bassett</title>
+        <title>Bryan James Bassett </title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
