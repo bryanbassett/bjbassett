95a6659134beca5d94e5545b881c0e55252156e5
bbassett <bbassett@kent.edu>
2019-09-24 08:04:43 -0400

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 clear
-echo "Now running BJBASSETT.ORG deploy script."
+echo "---Now running BJBASSETT.ORG deploy script."
 echo
-W
 echo
 git stash
 git pull
-echo "Pulled latest code"
+echo "---Pulled latest code or already up to date."
 php artisan cache:clear
 php artisan config:clear
 php artisan view:clear
-echo "Cleared all cache"
+echo "---Cleared all cache"
+echo
+echo
+echo "---Done."
