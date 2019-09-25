081a0b023d371343ab74880f5ee8018c1f189cbd
bbassett <bbassett@kent.edu>
2019-09-25 16:26:59 -0400

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ echo
 php artisan down
 git stash
 git pull
+clear
 echo "---Pulled latest code or already up to date."
 php artisan config:cache
 php artisan view:cache
@@ -14,4 +15,7 @@ echo "---Cleared all cache"
 echo
 echo
 echo "---Done."
+sudo chgrp -R www-data storage bootstrap/cache
+sudo chmod -R ug+rwx storage bootstrap/cache
 php artisan up
+echo "---Site now live again."
